40e5b59627cbf366b7d0fd4d63d1043fb170dec8
Salaruddin <salaruddinjalal@gmail.com>
2024-10-13 13:04:13 +0430

    Navbar is implemented. react-router-dom, and Material icons are installed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { ThemeContext } from "./context/theme.context";
+import Navbar from "./components/navbar/Navbar.component";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <div className={appStyles}>
+      <Navbar />
       <h1>helloworld</h1>
     </div>
   );
